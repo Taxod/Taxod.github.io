acab213332ec9b4c75440eaf08d6b297ff081ae2
Kun Chen <kchen@csie.org>
2017-07-30 21:41:39 +0800

    add chart

--- 3/js/index.js
+++ 3/js/index.js
@@ -4,254 +4,254 @@
     'thirty', 'thirty_one', 'thirty_two', 'thirty_three', 'thirty_four', 'thirty_five', 'thirty_six', 'thirty_seven', 'thirty_eight', 'thirty_nine',
     'forty', 'forty_one', 'forty_two', 'forty_three', 'forty_four', 'forty_five', 'forty_six', 'forty_seven', 'forty_eight', 'forty_nine', 'fifty'
 ];*/
-data = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
-    '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40'
-]
-data_row = ['.row-1', '.row-2', '.row-3', '.row-4', '.row-5', '.row-6', '.row-7', '.row-8']
+// data = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+//     '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40'
+// ]
+// data_row = ['.row-1', '.row-2', '.row-3', '.row-4', '.row-5', '.row-6', '.row-7', '.row-8']
 
-var reply = -3;
-$('#data').on('click', '#0', function() {
-    console.log('0');
-    reply = 0;
-    check();
-});
+// var reply = -3;
+// $('#data').on('click', '#0', function() {
+//     console.log('0');
+//     reply = 0;
+//     check();
+// });
 
-$('#data').on('click', '#1', function() {
-    console.log('1');
-    reply = 1;
-    check();
-});
+// $('#data').on('click', '#1', function() {
+//     console.log('1');
+//     reply = 1;
+//     check();
+// });
 
-$('#data').on('click', '#2', function() {
-    console.log('2');
-    reply = 2;
-    check();
-});
+// $('#data').on('click', '#2', function() {
+//     console.log('2');
+//     reply = 2;
+//     check();
+// });
 
-$('#data').on('click', '#3', function() {
-    console.log('3');
-    reply = 3;
-    check();
-});
-$('#data').on('click', '#4', function() {
-    console.log('4');
-    reply = 4;
-    check();
-});
-$('#data').on('click', '#5', function() {
-    console.log('5');
-    reply = 5;
-    check();
-});
-$('#data').on('click', '#6', function() {
-    console.log('6');
-    reply = 6;
-    check();
-});
-$('#data').on('click', '#7', function() {
-    console.log('7');
-    reply = 7;
-    check();
-});
+// $('#data').on('click', '#3', function() {
+//     console.log('3');
+//     reply = 3;
+//     check();
+// });
+// $('#data').on('click', '#4', function() {
+//     console.log('4');
+//     reply = 4;
+//     check();
+// });
+// $('#data').on('click', '#5', function() {
+//     console.log('5');
+//     reply = 5;
+//     check();
+// });
+// $('#data').on('click', '#6', function() {
+//     console.log('6');
+//     reply = 6;
+//     check();
+// });
+// $('#data').on('click', '#7', function() {
+//     console.log('7');
+//     reply = 7;
+//     check();
+// });
 
 
-$('#data').on('click', '#8', function() {
-    console.log('8');
-    reply = 8;
-    check();
-});
+// $('#data').on('click', '#8', function() {
+//     console.log('8');
+//     reply = 8;
+//     check();
+// });
 
-$('#data').on('click', '#9', function() {
-    console.log('9');
-    reply = 9;
-    check();
-});
+// $('#data').on('click', '#9', function() {
+//     console.log('9');
+//     reply = 9;
+//     check();
+// });
 
-$('#data').on('click', '#10', function() {
-    console.log('10');
-    reply = 10;
-    check();
-});
+// $('#data').on('click', '#10', function() {
+//     console.log('10');
+//     reply = 10;
+//     check();
+// });
 
-$('#data').on('click', '#11', function() {
-    console.log('11');
-    reply = 11;
-    check();
-});
+// $('#data').on('click', '#11', function() {
+//     console.log('11');
+//     reply = 11;
+//     check();
+// });
 
-$('#data').on('click', '#12', function() {
-    console.log('12');
-    reply = 12;
-    check();
-});
+// $('#data').on('click', '#12', function() {
+//     console.log('12');
+//     reply = 12;
+//     check();
+// });
 
-$('#data').on('click', '#13', function() {
-    console.log('13');
-    reply = 13;
-    check();
-});
+// $('#data').on('click', '#13', function() {
+//     console.log('13');
+//     reply = 13;
+//     check();
+// });
 
-$('#data').on('click', '#14', function() {
-    console.log('14');
-    reply = 14;
-    check();
-});
+// $('#data').on('click', '#14', function() {
+//     console.log('14');
+//     reply = 14;
+//     check();
+// });
 
-$('#data').on('click', '#15', function() {
-    console.log('15');
-    reply = 15;
-    check();
-});
+// $('#data').on('click', '#15', function() {
+//     console.log('15');
+//     reply = 15;
+//     check();
+// });
 
-$('#data').on('click', '#16', function() {
-    console.log('16');
-    reply = 16;
-    check();
-});
+// $('#data').on('click', '#16', function() {
+//     console.log('16');
+//     reply = 16;
+//     check();
+// });
 
-$('#data').on('click', '#17', function() {
-    console.log('17');
-    reply = 17;
-    check();
-});
+// $('#data').on('click', '#17', function() {
+//     console.log('17');
+//     reply = 17;
+//     check();
+// });
 
-$('#data').on('click', '#18', function() {
-    console.log('18');
-    reply = 18;
-    check();
-});
+// $('#data').on('click', '#18', function() {
+//     console.log('18');
+//     reply = 18;
+//     check();
+// });
 
-$('#data').on('click', '#19', function() {
-    console.log('19');
-    reply = 19;
-    check();
-});
+// $('#data').on('click', '#19', function() {
+//     console.log('19');
+//     reply = 19;
+//     check();
+// });
 
-$('#data').on('click', '#20', function() {
-    console.log('20');
-    reply = 20;
-    check();
-});
+// $('#data').on('click', '#20', function() {
+//     console.log('20');
+//     reply = 20;
+//     check();
+// });
 
-$('#data').on('click', '#21', function() {
-    console.log('21');
-    reply = 21;
-    check();
-});
+// $('#data').on('click', '#21', function() {
+//     console.log('21');
+//     reply = 21;
+//     check();
+// });
 
-$('#data').on('click', '#22', function() {
-    console.log('22');
-    reply = 22;
-    check();
-});
+// $('#data').on('click', '#22', function() {
+//     console.log('22');
+//     reply = 22;
+//     check();
+// });
 
-$('#data').on('click', '#23', function() {
-    console.log('23');
-    reply = 23;
-    check();
-});
+// $('#data').on('click', '#23', function() {
+//     console.log('23');
+//     reply = 23;
+//     check();
+// });
 
-$('#data').on('click', '#24', function() {
-    console.log('24');
-    reply = 24;
-    check();
-});
+// $('#data').on('click', '#24', function() {
+//     console.log('24');
+//     reply = 24;
+//     check();
+// });
 
-$('#data').on('click', '#25', function() {
-    console.log('25');
-    reply = 25;
-    check();
-});
+// $('#data').on('click', '#25', function() {
+//     console.log('25');
+//     reply = 25;
+//     check();
+// });
 
-$('#data').on('click', '#26', function() {
-    console.log('26');
-    reply = 26;
-    check();
-});
+// $('#data').on('click', '#26', function() {
+//     console.log('26');
+//     reply = 26;
+//     check();
+// });
 
-$('#data').on('click', '#27', function() {
-    console.log('27');
-    reply = 27;
-    check();
-});
+// $('#data').on('click', '#27', function() {
+//     console.log('27');
+//     reply = 27;
+//     check();
+// });
 
-$('#data').on('click', '#28', function() {
-    console.log('28');
-    reply = 28;
-    check();
-});
+// $('#data').on('click', '#28', function() {
+//     console.log('28');
+//     reply = 28;
+//     check();
+// });
 
-$('#data').on('click', '#29', function() {
-    console.log('29');
-    reply = 29;
-    check();
-});
+// $('#data').on('click', '#29', function() {
+//     console.log('29');
+//     reply = 29;
+//     check();
+// });
 
-$('#data').on('click', '#30', function() {
-    console.log('30');
-    reply = 30;
-    check();
-});
+// $('#data').on('click', '#30', function() {
+//     console.log('30');
+//     reply = 30;
+//     check();
+// });
 
-$('#data').on('click', '#31', function() {
-    console.log('31');
-    reply = 31;
-    check();
-});
+// $('#data').on('click', '#31', function() {
+//     console.log('31');
+//     reply = 31;
+//     check();
+// });
 
-$('#data').on('click', '#32', function() {
-    console.log('32');
-    reply = 32;
-    check();
-});
+// $('#data').on('click', '#32', function() {
+//     console.log('32');
+//     reply = 32;
+//     check();
+// });
 
-$('#data').on('click', '#33', function() {
-    console.log('33');
-    reply = 33;
-    check();
-});
+// $('#data').on('click', '#33', function() {
+//     console.log('33');
+//     reply = 33;
+//     check();
+// });
 
-$('#data').on('click', '#34', function() {
-    console.log('34');
-    reply = 34;
-    check();
-});
+// $('#data').on('click', '#34', function() {
+//     console.log('34');
+//     reply = 34;
+//     check();
+// });
 
-$('#data').on('click', '#35', function() {
-    console.log('35');
-    reply = 35;
-    check();
-});
+// $('#data').on('click', '#35', function() {
+//     console.log('35');
+//     reply = 35;
+//     check();
+// });
 
-$('#data').on('click', '#36', function() {
-    console.log('36');
-    reply = 36;
-    check();
-});
+// $('#data').on('click', '#36', function() {
+//     console.log('36');
+//     reply = 36;
+//     check();
+// });
 
-$('#data').on('click', '#37', function() {
-    console.log('37');
-    reply = 37;
-    check();
-});
+// $('#data').on('click', '#37', function() {
+//     console.log('37');
+//     reply = 37;
+//     check();
+// });
 
-$('#data').on('click', '#38', function() {
-    console.log('38');
-    reply = 38;
-    check();
-});
+// $('#data').on('click', '#38', function() {
+//     console.log('38');
+//     reply = 38;
+//     check();
+// });
 
-$('#data').on('click', '#39', function() {
-    console.log('39');
-    reply = 39;
-    check();
-});
+// $('#data').on('click', '#39', function() {
+//     console.log('39');
+//     reply = 39;
+//     check();
+// });
 
-$('#data').on('click', '#40', function() {
-    console.log('40');
-    reply = 40;
-    check();
-});
+// $('#data').on('click', '#40', function() {
+//     console.log('40');
+//     reply = 40;
+//     check();
+// });
 
 var getRandomColor = function() {
     r = Math.floor(Math.random() * 256); //0~255
@@ -303,6 +303,9 @@ var printBox = function() {
     var answer = Math.floor(Math.random() * 40);
     var color = getRandomColor();
     var ans_color = getAnswerColor(color);
+
+    $('#data').empty();
+
     //行
     for (var i = 0; i < 5; i++) {
         var $row = $('<div>').attr('data-row', i)
@@ -317,6 +320,20 @@ var printBox = function() {
             } else {
                 $button.attr('data-answer', false).css('background-color', color.rgb)
             }
+
+            $button.on('click', function() {
+                var ans = $(this).attr('data-answer');
+                if (ans === "true") {
+                    var score = +$('#score').text();
+                    printBox();
+                    $('#score').text(score + 1);
+                } else {
+                    $('#game').hide();
+                    $('#chart').show();
+                    showChart();
+                }
+            })
+
             var $col = $row.append($button)
             $('#data').append($col)
         }
@@ -334,18 +351,18 @@ var printhintjpg = function() {
 }
 
 
-var r1, g1, b1;
-var r2, g2, b2;
-var rgb1, rgb2;
+// var r1, g1, b1;
+// var r2, g2, b2;
+// var rgb1, rgb2;
 var score = 0;
 
-var choosechangecolor;
+// var choosechangecolor;
 
 
-var answer;
-var randomanswer = function() {
-    answer = Math.floor(Math.random() * 40); //0~39
-}
+// var answer;
+// var randomanswer = function() {
+//     answer = Math.floor(Math.random() * 40); //0~39
+// }
 
 // $(document).ready(function() {
 //     // 按start後滑動至id = data
@@ -358,84 +375,102 @@ var randomanswer = function() {
 // });
 
 //避免多次start
-var void_restrat = 0;
-$('#start').on('click', function() {
-    // if (void_restrat == 0) {
-    //     score = 0;
-    //     document.getElementById("text").innerHTML = 'score : ' + score;
-    //     randomanswer();
-    // randomcolor();
-    // changeColor();
-    printBox();
-    //     printhintjpg();
-    //     void_restrat = 1;
-    // } else {
-    //     alert('Already Start!!!')
-    // }
 
-})
+// var check = function() {
+//     if (reply == answer) {
+//         score += 1;
+//         for (var j = 0; j < data_row.length; j++) {
+//             $(data_row[j]).empty();
+//         }
+//         randomanswer();
+//         randomcolor();
+//         changeColor();
+//         printbox();
+//         document.getElementById("text").innerHTML = 'score' + score;
+//     } else {
+//         //console.log(0);
+//         void_restrat = 0;
+//         for (var j = 0; j < data_row.length; j++) {
+//             $(data_row[j]).empty();
+//         }
+//         hint_used = 0;
+//         var hintid;
+//         for (var j = 0; j < 3; j++) {
+//             hintid = '#hint' + j;
+//             $(hintid).remove();
+//         }
 
-document.getElementById("text").innerHTML = 'score : ' + score;
+//     }
+// }
 
-var check = function() {
-    if (reply == answer) {
-        score += 1;
-        for (var j = 0; j < data_row.length; j++) {
-            $(data_row[j]).empty();
+// var hint_used = 0;
+// $('#hint').on('click', function() {
+//     var answer_name = '#' + answer;
+//     $(answer_name).attr('style', 'width:70px; height:70px; font-size:20px;border-width:medium; border-style:dashed; border-width:1px; padding: 50px ;background-color:rgb' + rgb2 + ';');
+//     //console.log(answer_name + '!!!!!!');
+//     if (hint_used < 3) {
+//         var hintid;
+//         hintid = '#hint' + hint_used;
+//         $(hintid).attr('src', './picture/hint2.jpg').attr('style', 'width:50px ; height:50px;');
+//         hint_used += 1;
+//     } else {
+//         alert('提示用完了  HAHA 找不到齁')
+//     }
+
+// })
+
+var showChart = function() {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
-        randomanswer();
-        randomcolor();
-        changeColor();
-        printbox();
-        document.getElementById("text").innerHTML = 'score' + score;
-    } else {
-        //console.log(0);
-        void_restrat = 0;
-        for (var j = 0; j < data_row.length; j++) {
-            $(data_row[j]).empty();
-        }
-        hint_used = 0;
-        var hintid;
-        for (var j = 0; j < 3; j++) {
-            hintid = '#hint' + j;
-            $(hintid).remove();
-        }
-
-    }
+    });
 }
 
-var hint_used = 0;
-$('#hint').on('click', function() {
-    var answer_name = '#' + answer;
-    $(answer_name).attr('style', 'width:70px; height:70px; font-size:20px;border-width:medium; border-style:dashed; border-width:1px; padding: 50px ;background-color:rgb' + rgb2 + ';');
-    //console.log(answer_name + '!!!!!!');
-    if (hint_used < 3) {
-        var hintid;
-        hintid = '#hint' + hint_used;
-        $(hintid).attr('src', './picture/hint2.jpg').attr('style', 'width:50px ; height:50px;');
-        hint_used += 1;
-    } else {
-        alert('提示用完了  HAHA 找不到齁')
-    }
+var Start = function() {
+    $('#chart').hide();
+    $('#game').show();
 
+    $('#start').on('click', function() {
+        $('#score').text('0');
+        printBox();
+        printhintjpg();
+    })
+}
+
+
+$(document).ready(function() {
+    Start();
 })
-
-/*
-
-$('#data').on('click', '#one', function() {
-    var $button;
-    $button = $('<button>').attr('style', 'width:50px; height:50px; font-size:20px;');
-    console.log("great!");
-
-    $('#data').append($button);
-});*/
-
-/*
-var r, g, b;
-var rgb;
-var test = function() {
-    r = 50;
-    g = 100;
-    b = 150;
-    rgb = '(' + r + ',' + g + ',' + b + ')'
-}*/
